2645fcdf1128954c5769ec73fe4ff72e803dc03e
Angel D. Munoz <scythes.lol@gmail.com>
2017-10-08 18:39:03 -0500

    fix "app.listen Is not a function" error

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,13 @@ import * as bodyParser from 'body-parser';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './modules/app.module';
 
-const instance = express();
-instance.use(bodyParser.json());
-
-const app = NestFactory.create(ApplicationModule, instance);
-app.listen(3000, () => console.log('Application is listening on port 3000'));
+(async function start() {
+  const instance = express();
+  instance.use(bodyParser.json());
+  try {
+    const app = await NestFactory.create(ApplicationModule, instance);
+    app.listen(3000, () => console.log('Application is listening on port 3000'));
+  } catch (error) {
+    console.error(error);
+  }
+})();
